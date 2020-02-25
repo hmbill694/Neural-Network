dc9125a1ffcf24da8bed4da719c000ea23eb7446
Harrison <hmbillings98@gmail.com>
2020-02-25 14:45:27 -0700

    Final Commit

---
+++ NeuralNetwork.py
@@ -0,0 +1,199 @@
+import random
+import math
+import numpy as np
+from sklearn.externals import joblib
+
+
+class NeuralNetwork:
+    def __init__(self, arch, inputs, outputs, learning_rate):
+        self.arch = arch
+        self.input_pairs = [(x, y) for x, y in zip(inputs, outputs)]
+        self.weights = self.__create_weights(arch)
+        self.biases = self.__create_biases(arch)
+        self.error = None
+        self.learning_rate = learning_rate
+        self.activations = None # All neuron outputs from the network
+        self.output = None
+        self.training_set = None
+        self.testing_set = None
+        self.accuracy = None
+
+    def __sigmoid(self, x):
+        """This is the activation function for the network, it will
+            squishify the results of the network to be between -1 and 1.
+            It is non-linear  like all other activation functions"""
+        return 1 / (1 + np.exp(-x))
+
+    def __sigmoid_prime(self, x):
+        """This is the "derivative" of the sigmoid function.
+            it will be used in the learning phase of the network.
+            The real derivative of the sigmoid function is
+            f'(x) = sigmoid(x) x (1- sigmoid(x)). However
+            when using this in the program my values have already
+            gone through the sigmoid function so doing so again would be
+            incorrect"""
+        return x * (1 - x)
+
+    def __create_weight_layer(self, row, col):
+        """This will create a matrix with random values of the shape
+                R x C where R = the number of neurons in the next layer
+                and C = the number of neurons in the current layer"""
+
+        return np.random.randn(row, col)
+
+    def __create_weights(self, arch):
+        """This function will create the weight matrix for an entire network
+                it takes a list where each member in the list represents the the number
+                of neurons in that layer. So a input of [5,2,1] represents a network
+                with 5 neurons in the first layer, 2 in the second and one in the last layer.
+                It will return a list of n-dimensional numpy arrays where each entry in the list
+                represents the weights from one layer to the next"""
+
+        # Okay so what's this slicing about? For the architecture of a network we are able to
+        # deduce the overall structure of the weight matrix. We will have n-1 weight layers
+        # per network where n is the total number of network layers. There are no weights for
+        # incoming data so a network shaped like [5,2,1] will have 2 weight layers.
+        # The first weight layer will be an array with two rows and 5 columns, the second
+        # 1 row with 2 columns
+        column_values = arch[:-1]
+        row_values = arch[1:]
+
+        return [self.__create_weight_layer(row, col) for row, col in zip(row_values, column_values)]
+
+    def __create_bias_layer(self, layer):
+        """Creates a np matrix of (n x 1) for a layer of a network
+                returns the matrix """
+        return np.random.randn(layer, 1)
+
+    def __create_biases(self, arch):
+        """The number of bias matrices for a network is n - 1, where
+                n is the number of layers in the network, including inputs.
+                Each neuron in a layer will have it's own bias. So a network
+                of size [5,2,1] will have two biases for one layer and 1 for the next.
+                The inputs do not get biases."""
+        layers_that_need_biases = arch[1:]
+        return [self.__create_bias_layer(neurons_in_layer) for neurons_in_layer in layers_that_need_biases]
+
+    def __feed_forward(self, input_matrix_as_list, guess=False):
+        """This function feeds the inputs into the matrix."""
+        # Store inputs as a column matrix (n x 1)
+        if isinstance(input_matrix_as_list, (np.ndarray, np.generic)):
+            input_matrix_as_list = input_matrix_as_list.tolist()
+
+        output = np.c_[input_matrix_as_list]
+        layer_activations = [output]
+
+        for bias, weight in zip(self.biases, self.weights):
+            output = self.__sigmoid(np.dot(weight, output) + bias)
+            layer_activations.append(output)
+
+        if not guess:
+            self.output = output
+            self.activations = layer_activations
+        else:
+            return output
+
+    def __back_propagate_error(self, actual_values_matrix, guessed_values_matrix):
+        """This function will calculate a error matrix for each hidden layer neuron """
+        actual_values_matrix = np.c_[actual_values_matrix]
+        guessed_values_matrix = np.c_[guessed_values_matrix]
+
+        # seed error with error between output of network and target output
+        error = [np.subtract(actual_values_matrix, guessed_values_matrix)]
+
+        # Reverse the weight matrix and then iterate in reverse to
+        # the last layer, we omit the first layer as that would calculate
+        # the error for inputs which does not make sense
+        for index, layer in enumerate(self.weights[:0:-1]):
+            error.append(np.dot(layer.transpose(), error[index]))
+        self.error = list(reversed(error))
+
+    def __create_deltas(self):
+        """This method will find the changes to each of the weights and biases in the network
+            it will then return these two lists of deltas"""
+
+        # lambda function to apply the derivative of sigmoid to the activations array
+        def vectorized_sigmoid_prime(x): return self.__sigmoid_prime(x)
+
+        # lists to contain the delta for each layer's weights and biases
+        delta_weights = []
+        delta_biases = []
+
+        # the gradients found from applying the sigmoid prime to the activations, omitting the first layer
+        # which is the inputs, these cannot be altered
+        gradients = [vectorized_sigmoid_prime(layer) for layer in self.activations[1:]]
+
+        # iterate over the errors, gradients, weights, biases, and activations
+        # applying the function delta_weight_layer = Gradient * lr * Error Layer * activation.T
+        # the delta_biases = gradients for that layer
+        for _, error_layer, gradient, activation, bias in zip(self.weights, self.error, gradients, self.activations,
+                                                              self.biases):
+            gradient = np.multiply(gradient, self.learning_rate)
+            gradient = np.multiply(gradient, error_layer)
+            delta_layer = np.multiply(activation.transpose(), gradient)
+            delta_weights.append(delta_layer)
+            delta_biases.append(gradient)
+
+        # return lists of gathered errors
+        return delta_weights, delta_biases
+
+    def train_SGD(self, iterations, train_set_size):
+        """This method will uses stochastic gradient descent to train the network. It will
+            select a random entry in the training set, which is tuple of lists where [0]: inputs
+            and where [1]: 1 is the known answer."""
+
+        self.__generate_test_training_sets(train_set_size)
+
+        # training loop for SGD
+        for x in range(iterations):
+            test_item_pair = random.choice(self.training_set)
+            known_output = test_item_pair[1]
+            self.__feed_forward(test_item_pair[0])
+            self.__back_propagate_error(known_output, self.output)
+            delta_weight_matrix, delta_biases_matrix = self.__create_deltas()
+
+            # adjust weights and biases by delta for the layer
+            self.weights = np.add(self.weights, delta_weight_matrix)
+            self.biases = np.add(self.biases, delta_biases_matrix)
+
+    def __generate_test_training_sets(self, train_set_size):
+        """This method will create the testing and training sets for the network
+            to test accuracy. These lists are built from the list of all input pairs.
+            This set of pairs will be constrained with a percentage
+            of the total set. This percentage is represented as train_set_size. So for example
+            if this method is set to preform with a training set size of .7 then 
+            It will randomly select values from 70% of the list and reserve 30% to test on later. 
+            """
+        if train_set_size <= 0.0 or isinstance(train_set_size, int):
+            raise ValueError('Training set size must be a positive floating point number')
+
+        # find index corresponding to percentage of desired training set
+        split_index = math.floor(len(self.input_pairs) * train_set_size)
+
+        # split self.input_pairs along percentage
+        self.training_set = self.input_pairs[:split_index]
+        self.testing_set = self.input_pairs[split_index:]
+
+    def guess(self, input_list):
+        """Given a sample input list show what the network would output"""
+        return self.__feed_forward(input_list, True).transpose().flatten()
+
+    def test_network(self):
+        total_tests = len(self.testing_set)
+        total_correct = 0
+        vectorized_normalize = np.vectorize(lambda x: 1.0 if x >= .5 else 0.0)
+        for member in self.testing_set:
+            guess = self.guess(member[0])
+            guess = vectorized_normalize(guess)
+            if np.array_equal(guess, member[1]):
+                total_correct += 1
+
+        self.accuracy = total_correct / total_tests
+
+    @staticmethod
+    def save_network(network, file_name):
+        joblib.dump(network, file_name)
+
+    @staticmethod
+    def load_network(file_name):
+        return joblib.load(file_name)
---
+++ README.md
@@ -0,0 +1,63 @@
+# Neural Network
+
+This is my attempt at creating a library from scratch that builds feedforward neural networks to help understand more deeply how they work. I have developed this concurrently with my Machine Learning Lab application where I will, fingers crossed, be able to deploy this to a full stack app. It is my hope that anyone who comes across this can look at the code project along with this readme and feel like they at least have some idea where to start when it come to trying replicate something like this. 
+
+## My Goal
+I learn best through having a goal so I decided to attempt to build a network that would be able to classify the the sklearn iris dataset with above 95% accuracy. 
+
+## Resources 
+I could not have attempted this without the wonderful resources made available to the community. Some of the most helpful resources for me include [Professor Daniel Shiffman's Youtube Series](https://www.youtube.com/watch?v=XJ7HLz9VYz0&list=PLRqwX-V7Uu6aCibgK1PTWWu9by6XFdCfh) where he explains the mathmatics behind and builds a 3 layer neural network in JavaScript. Another fantastic resource was the [Video series by 3Blue1Brown](https://www.youtube.com/watch?v=aircAruvnKk&list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi) where the theory of Neural Networks is discussed in depth. Another fantastic reference is [Neural Networks And Deep Learning](http://neuralnetworksanddeeplearning.com/index.html) by Michael Nielsen. If you, like I did,  would like to bone up on some of the  math concepts required like Linear Algebra (matrix layouts, scalar multiplication, element-wise addition, dot-product) then check out Khan Academy's [Linear Algebra Playlist](https://www.youtube.com/watch?v=xyAuNHPsq-g&list=PLFD0EB975BA0CC1E0). In addition there is some Calculus concepts like gradients and the chain rule that you should feel comfortable with. Again, Khan Acadmey comes in clutch to assist with their [Calculus Playlists](https://www.youtube.com/watch?v=TrcCbdWwCBc&list=PLSQl0a2vh4HC5feHa6Rc5c0wbRTx56nF7)
+
+## How Neural Networks Work
+See one, do one, teach one is the motto so I'm gonna try to give you a overview of how these things work. I'm first going to cover what the architecture a network is. I'm then going to cover how the network can "learn". Finally, I'm going to go over one way to train a network. There is much more commentary within the comments in the code, so check that out as well.
+
+### Architecture 
+So what do networks even look like? 
+![image](https://user-images.githubusercontent.com/46241098/75271864-2127a000-57ba-11ea-9e36-77251ea3872b.png)
+
+Notice the little circles, which are called neurons and lines, which I'm going to refer to as weights. Look at how all the neurons in each layer have connections, that are associated with the weights, to every node in the next layer. These weights along with biases (discussed later) are tuned to help the network learn.
+
+A network has 3 kinds of layer, an input layer (the first layer), an output layer (the last layer), and every layer inbetween is called a hidden layer. If we imagine a 3 layer network. Meaning there is an input layer which i'm going to call I, one hidden layer of  neurons which I will call layer H and an output layer, which is denoted as layer O. The data will flow through like this The all inputs in layer I will be feed into every neuron in layer H. Every output produced by by the neurons in layer H, will be feed into every neuron in layer I. The outputs of the nuerons in layer I represent the answer of the network. 
+
+This nature of forward flow of the inputs through the network is why we call this a feed forward network. So what are the neurons? Neurons are a function, meaning they take input, preform some operation 
+on the input, and return an output. Yeah, sick but what function are the neurons preforming, what are they doing it on, and what's the output look like? 
+
+Neurons preform a weighted sum using all inputs and adds a bias, then applies some function called the activation function to that sum, and then returns a single output. This output becomes an input 
+for the next layer in the network. Nuerons in every layer execpt the first will do this operation. I'll break down the bias, and the activation function since we've already covered the weights.
+
+The bias described above is simply a number that will be added to every sum. Every neuron except the input neurons have a bias associated with them. 
+
+The activation function is a function that will be able to map the weighted sum plust that bias to a value in the range [-1 , 1].
+
+### Learning
+
+**Assume all of these are matrices**
+
+So when we say a network is learning what does that mean? Really this is the process of comparing a known answer to the answer that the network output. Then this error is used to calculate the changes to be made to the weights and biases. 
+
+Calculating the error is easy for the output layer we just subtract the predicted value from the actual value. But then the question becomes well how do I get the error for the rest of the layers. Well the error for all neurons in a layer is the dot product of the transposed weights matrix for the layer and the error of the neurons for the layer prior. 
+
+Next we will calculate the amount that we should adjust the weights and biases for a single layer. Before we do this we must introduce the concept of the learning rate and talk about derivaties for a second. A learning rate is a scalar multiplier that well temper the size of adjustment that we will make to weights and biases. Next for this step we will need to feed all outputs from a layer throught the derivative of the activation function for the layer, then multiply the result by the learning rate and the error for the layer. The deltas for the weights in a layer is the dot product of the transposed wieghts for a layer and the gradients calculated earlier. To adjust the weights and biases add the weights to the deltas and the biases to the gradients. 
+
+This is a gross over simplifiication and is explained much more in the references above which you should check out. 
+
+
+### Training
+Stochastic Gradient Descent! Man what a mouth full. We will be training our network using this method. To do this we will be, over a set number of iterations, randomly picking a element from a subset of the inputs and outputs. Adjusting the weights and biases like we talked about previously, and evaluating our network using the remainder of the inputs and outputs to test how many our network correctly identifies. This is a much more indepth topic, but for the purposes of making the network this is all the intuiton needed. 
+
+
+
+## The Technologies Used
+
+While it was my intention to build this without using a complete framework I still usedseveral popular python libraries to assist with this goal. I mean come on, I'm not actuallygonna do this all from scratch. This whole project was worked
+out in tested in a jupyter Notebook. I used sklearn's datasets and joblib library to get my dataset and subsequently to save my network after I had finished. I used both numpy to handle the actualy matrix storage and mathmatical manipulations
+and manipulations and pandas to assist with storing data. 
+
+## Dependencies 
+To get the class working you will need sklearn and numpy. I do recommend installing pandas as well if you intend to implement the Network class. Also I would recommend installing Jupyter Notebook to play around with the class in a live environment.
+```sh
+$ pip install notebook
+$ pip install numpy
+$ pip install scikit-learn
+$ pip install pandas
+```
